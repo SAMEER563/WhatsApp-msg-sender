6ae95556344e5e69e9586b7b1eef77dc5926316e
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2023-07-29 08:34:12 +0530

    initial commit

---
+++ whatsapp.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+time.sleep(4)
+
+count=0
+while count<=50:
+
+ 
+
+  pyautogui.typewrite("Good Evening "+str( )) 
+  pyautogui.press("enter")
+  count=count+1
+ 
+
+ 
+ 
